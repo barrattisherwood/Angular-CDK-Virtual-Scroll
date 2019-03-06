4bb6e2ab1fdf08b0d90db5896c6c90d9bc283e47
BIsherwood <barratti@osiristrading.co.za>
2019-03-06 11:50:00 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'virtual-scroll-app';
+  numbers: number[] = [];
+  title = 'Virtual Scroll Prototype';
+
+  constructor() {
+    for (let index = 0; index < 10000; index++) {
+      this.numbers.push(this.randomIntFromInterval(1, 100));
+    }
+  }
+
+  randomIntFromInterval(min,max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
 }
