e4f6057f1127bd43c9f94c567a44e47b6373075f
BIsherwood <barratti@osiristrading.co.za>
2019-03-07 11:01:15 +0200

    refactor(layout): height calculation using VH

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  @ViewChild('gridtemplate') elementView: ElementRef;
   numbers: number[] = [];
   title = 'Virtual Scroll Prototype';
+  public gridHeight = 300;
 
   constructor() {
+    console.log('elementView', this.elementView);
     for (let index = 0; index < 10000; index++) {
       this.numbers.push(this.randomIntFromInterval(1, 100));
     }
+  }
+
+  ngOnInit() {
+    debugger;
+    this.resize();
+  }
+
+  public resize(): void {
+    // The Grid height is actually applied to the content area, not as total
+    // This is **a bug** that will be fixed soon.
+    this.gridHeight = this.elementView.nativeElement.offsetHeight - 100;
   }
 
   randomIntFromInterval(min,max) {
